6581ea7806b3769784bead07fda0c8b805dab24d
jeonghyeon-05 <jh5452965@gmail.com>
2026-03-17 16:39:00 +0900

    hi

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
 <?php
-/* Nothing to see here. */
+/* If, switch, match */
+
+$stock = 5;
+$ordered = 3;
+
+if ($stock >0) {
+    $message = "In stock.";
+} elseif ($ordered > 0) {
+    $message = "Coming soon";
+} else {
+    $message = "Sod out!";
+}
+
+//SWITCH문
+$day = 'Tuesday';
+
+switch($day) {
+    case 'Monday' :
+        $offer = '20% off chocolates';
+        break;
+    case 'Tuesday' :
+        $offer = '20% off mints';
+        break;
+    default :
+        $offer = '3+1 mints';
+}
+
+//Match문
+$month = 'March';
+
+$mth_offer = match($month) {
+    'January, February' => '20% off mints',
+    'March,'            => '30% off chocolates',
+    default             => '10% off whole order',
+}
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>If, switch, match</title>
+        <link rel = "stylesheet" href = "css/styles.css">
+    </head>
+    <body>
+        <h1>The Candy Store</h1>
+        <h2>Chocolate</h2>
+        <p><?= $message ?></p>
+
+        <h2>Offers on <?= $day; ?></?>
+        <p><?= $offer; ?></p>
+
+        <h2>Offers on <?= $month; ?></?>
+        <p><?= $mth_offer; ?></p>
+    </body>
+</html>
---
+++ loops.php
@@ -0,0 +1,72 @@
+<!-- loops.php-->
+
+<?php
+//WHILE
+$counter = 10;
+$packs = 5;
+$price = 1.99;
+
+//Do...WHILE
+
+//FOR
+
+//FOREACH
+$products = [
+    'Toffee'    => 2.99,
+    'Mints'     => 1.99,
+    'Fudge'     => 3.49,
+];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>while, for, foreach</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <h1>The Candy Store</h1>
+        <h2>Chocolate</h2>
+             <ol>
+            <?php
+        do {
+            echo '<li>';
+            echo "$counter; packs cost \$";
+            echo $price * $counter;
+            echo '</li>';
+            $counter++;
+        }while ($counter <= $packs);
+        ?>
+        </ol>
+
+        <h2>Mints</h2>
+             <ul>
+            <?php
+        for ($i = 0; $i <= 100; $i+=10){
+            echo '<li>';
+            echo "$counter; packs cost $";
+            echo $price * $i;
+            echo '</li>';
+            $counter++;
+        }
+        ?>
+        </ul>
+
+        <h2>Offers on <?= $day ?></h2>
+        <p><?= $offer; ?></p>
+
+        <h2>$products</h2>
+        <table>
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+            </tr>
+            <?php foreach ($products as $item => $price) { ?>
+                <tr>
+                    <td><?= $item; ?></td>
+                    <td><?= $price; ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
